47d6d28f683acdd9a7c25b9092c9b5c53a96c289
mauriciomolinapicco <mauriscode@gmail.com>
2025-02-26 20:03:14 -0300

    added info for about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,19 +2,23 @@ import RevealOnScroll from "../RevealOnScroll";
 
 
 function About() {
-
-
-    const backendSkills = ['Python', 'Django', 'FastAPI', 'SQL', 'Docker', 'AWS'];
-    const frontendSkills = ['Javascript', 'React', 'Tailwind']
+    const backendSkills = ['Python', 'Django', 'FastAPI', 'SQL', 'Docker', 'AWS', 'RESTful APIs'];
+    const frontendSkills = ['Javascript', 'React', 'Tailwind', 'HTML', 'CSS']
 
     return <section id="about" className="min-h-screen flex items-center justify-center py-20">
         <RevealOnScroll>
-        <div className="max-w-3xl mx-auto px-4">
-            <h2 className="text-3xl font-bold md-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center">About me</h2>
+        <div className="max-w-4xl mx-auto px-4">
+            <h2 className="text-3xl font-bold md-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center mb-8">About me</h2>
         
             <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                <p className="text-gray-300 mb-6s">
-                    Passionate developer with expertise in building the best software solutions
+                <p className="text-gray-300 mb-6">
+                <p className="mb-4">Hi, I'm Mauricio Molina Picco, a Full Stack Engineer with a passion for creating efficient, scalable web applications. I specialize in backend development using Python and Django or FastAPI, building reliable systems with PostgreSQL and AWS. On the frontend, I craft seamless, responsive user interfaces with React and Tailwind CSS, ensuring a smooth experience across devices.</p>
+                
+                <p className="mb-4">My expertise spans across developing and integrating complex APIs, deploying applications to cloud environments, and optimizing workflows for high-performance solutions. I thrive in collaborative settings, always aiming to improve code quality and deliver impactful results.</p>
+
+                <p className="mb-4">I'm constantly learning, staying up to date with the latest tech trends, and eager to tackle new challenges that push the boundaries of web development.</p>
+
+
                 </p>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -45,29 +49,47 @@ function About() {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
                 <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold">📖 Education </h3>
+                        <h3 className="text-xl font-bold mb-4">📖 Education </h3>
                         <ul className="list-disc list-inside text-gray-300 space-y-2">
-                            <li>
-                                <strong>Software Engineering</strong> - Universidad de Palermo - Buenos Aires
+                            <li className="font-thin">
+                                <strong className="font-bold">Software Engineering</strong>
                             </li>
-                            <li>
-                                <strong>Information Systems Engineering</strong> - Unviersidad Tecnologica Nacional - Cordoba
+                                <p className="pl-8">
+                                Universidad de Palermo - Buenos Aires (2024, present)
+                                </p>
+
+                            <li className="font-thin">
+                                <strong className="font-bold">Information Systems Engineering</strong>
                             </li>
-                            <li>
-                                <strong>Relevant Coursework:</strong> - Data Structures, Web Development, Software Arquitechture
+                                <p className="pl-8">
+                                    Unviersidad Tecnologica Nacional - Cordoba (2022, 2024)
+                                </p>
+
+                            <li className="font-thin">
+                                <strong className="font-bold">Relevant Coursework</strong>
                             </li>
+                                <p className="pl-8">
+                                    Data Structures, Web Development, Software Arquitechture
+                                </p>
                         </ul>
                 </div>
                 <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold">💼 Work Experience </h3>
+                        <h3 className="text-xl font-bold mb-4">💼 Work Experience </h3>
                         <div className="space-y-4 text-gray-300">
                             <div>
                                 <h4 className="font-semibold">Full Stack Developer at Rooster Software Barn</h4>
-                                <p>Developed and maintained full stack web applications</p>
+                                <p className="font-thin mt-2">Developed full-stack web applications using Python (Django) for the backend and JavaScript for the frontend, ensuring efficient user, 
+                                    payment, and data management.</p> 
+                                <p className="font-thin mt-2"> Deployed on Heroku, with AWS storage and PostgreSQL for scalability. Integrated third-party APIs like 
+                                    SendGrid and Stripe to enhance functionality and reliability.
+                                </p>
                             </div>
+                            <hr />
                             <div>
                                 <h4 className="font-semibold">Independant software developer - Freelancer</h4>
-                                <p>Developed and maintained full stack web applications</p>
+                                <p className="font-thin mt-2">Developed and maintained multiple web applications from the ground up using Django, Node.js, and Java, creating robust systems and seamless user experiences. 
+                                    Handled user authentication and data management with Django, PostgreSQL, and MySQL, ensuring security and reliability. Followed clean code principles 
+                                    and best practices to build scalable, maintainable systems. Managed version control with Git and utilized AWS for scalable storage and deployment solutions</p>
                             </div>
                         </div>
                 </div>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,15 +9,37 @@ function Home() {
             </h1>
 
             <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-                Soy un desarrollador full stack jfdsak fjdklsfjlkfdsafj adlksfj lksaf adsljflkadsfasjlfasdfkslka
+            Full Stack Engineer with expertise in Python, Django, and modern web technologies. I focus on delivering scalable applications and driving impactful solutions
             </p>
             <div className="flex justify-center space-x-4">
-                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
-                    hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">View Projects</a>
+                <a href="#contact" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
+                    hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Contact me</a>
                 
                 <a href="#projects" className="border border-blue-500/50 text-blue-100 py-3 px-6 rounded font-medium transition-all duration-200 
                     hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] hover:bg-blue-500/10">View Projects</a>
+            </div>
 
+
+            <div className="flex justify-center mt-10">
+                <a href="https://github.com/mauriciomolinapicco" className="w-18 h-16">
+                            <svg className="w-12 h-12 transition-all duration-200 hover:translate-y-0.5" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 256 256">
+                                        <g fill="#e9e9e9" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
+                                            <g transform="scale(8.53333,8.53333)">
+                                            <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,5.623 3.872,10.328 9.092,11.63c-0.056,-0.162 -0.092,-0.35 -0.092,-0.583v-2.051c-0.487,0 -1.303,0 -1.508,0c-0.821,0 -1.551,-0.353 -1.905,-1.009c-0.393,-0.729 -0.461,-1.844 -1.435,-2.526c-0.289,-0.227 -0.069,-0.486 0.264,-0.451c0.615,0.174 1.125,0.596 1.605,1.222c0.478,0.627 0.703,0.769 1.596,0.769c0.433,0 1.081,-0.025 1.691,-0.121c0.328,-0.833 0.895,-1.6 1.588,-1.962c-3.996,-0.411 -5.903,-2.399 -5.903,-5.098c0,-1.162 0.495,-2.286 1.336,-3.233c-0.276,-0.94 -0.623,-2.857 0.106,-3.587c1.798,0 2.885,1.166 3.146,1.481c0.896,-0.307 1.88,-0.481 2.914,-0.481c1.036,0 2.024,0.174 2.922,0.483c0.258,-0.313 1.346,-1.483 3.148,-1.483c0.732,0.731 0.381,2.656 0.102,3.594c0.836,0.945 1.328,2.066 1.328,3.226c0,2.697 -1.904,4.684 -5.894,5.097c1.098,0.573 1.899,2.183 1.899,3.396v2.734c0,0.104 -0.023,0.179 -0.035,0.268c4.676,-1.639 8.035,-6.079 8.035,-11.315c0,-6.627 -5.373,-12 -12,-12z"></path>
+                                            </g>
+                                        </g>
+                            </svg>
+                </a>
+
+                <a href="https://www.linkedin.com/in/mauricio-molina-picco/" className="w-18 h-16 transition-all duration-200 hover:translate-y-0.5">
+                <svg className="w-12 h-12" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 256 256">
+                        <g fill="#e9e9e9" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
+                            <g transform="scale(5.12,5.12)">
+                            <path d="M41,4h-32c-2.76,0 -5,2.24 -5,5v32c0,2.76 2.24,5 5,5h32c2.76,0 5,-2.24 5,-5v-32c0,-2.76 -2.24,-5 -5,-5zM17,20v19h-6v-19zM11,14.47c0,-1.4 1.2,-2.47 3,-2.47c1.8,0 2.93,1.07 3,2.47c0,1.4 -1.12,2.53 -3,2.53c-1.8,0 -3,-1.13 -3,-2.53zM39,39h-6c0,0 0,-9.26 0,-10c0,-2 -1,-4 -3.5,-4.04h-0.08c-2.42,0 -3.42,2.06 -3.42,4.04c0,0.91 0,10 0,10h-6v-19h6v2.56c0,0 1.93,-2.56 5.81,-2.56c3.97,0 7.19,2.73 7.19,8.26z"></path>
+                            </g>
+                        </g>
+                        </svg>
+                </a>
             </div>
         </div>
     </RevealOnScroll>
